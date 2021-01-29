9546f2c631d4ddb68eafff1bb7c90b3526bc5eed
griezma <griezma@outlook.com>
2021-01-29 15:59:49 +0100

    security config added

--- src/main/java/griezma/mssc/beerworksregistry/DiscoveryService.java
+++ src/main/java/griezma/mssc/beerworksregistry/DiscoveryService.java
@@ -6,8 +6,8 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
-public class ServiceRegistry {
+public class DiscoveryService {
     public static void main(String[] args) {
-        SpringApplication.run(ServiceRegistry.class, args);
+        SpringApplication.run(DiscoveryService.class, args);
     }
 }
